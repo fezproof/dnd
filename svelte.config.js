6795616cd03b41d8153e7b33c1a9ad01408621d4
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-07 22:58:58 +0800

    Added continuous deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,35 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+const kit = {
+	adapter: adapter(),
+	// hydrate the <div id="svelte"> element in src/app.html
+	target: '#svelte'
+};
+
+if (!dev) {
+	kit.paths = {
+		base: '/dnd',
+		assets: '/dnd'
+	};
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        "postcss": true
-    })],
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+	kit
 };
 
 export default config;
 // Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
-const mode = process.env.NODE_ENV;
-const dev = mode === "development";
-process.env.TAILWIND_MODE = dev ? "watch" : "build";
+
+process.env.TAILWIND_MODE = dev ? 'watch' : 'build';
