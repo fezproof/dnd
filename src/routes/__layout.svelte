4d21f0c8c999d40c64e6b1e44ae156f35d764784
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-08 01:37:09 +0800

    Add head tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,12 @@
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<title>Eos Rising: D&D campaign</title>
+	<meta
+		name="description"
+		content="Eos Rising is a home Dungeon and Dragons game, and this website has been created to help me and my players"
+	/>
+</svelte:head>
+
 <slot />
