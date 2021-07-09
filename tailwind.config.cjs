1f3ebb0e853bbe2b13b09563a358d8ddde56f01e
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-09 21:24:37 +0800

    Added some fonts and colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const config = {
 	mode: 'jit',
 	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	// darkMode: 'media',
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				green: {
+					main: '#0C3537',
+					light: '#6CC5AE'
+				}
+			},
+			fontFamily: {
+				serif: ['Abhaya Libre', ...defaultTheme.fontFamily.serif]
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/typography')]
 };
