9e18e98bdfcd4c2c2538ccd92105da6455c703f2
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-10 02:11:30 +0800

    Fixed nav width

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,7 +3,10 @@
 	import D20 from '$lib/svgs/d20.svelte';
 </script>
 
-<a href={`${base}/`} class="h-32 px-6 pt-4 pb-4 flex items-center justify-start uppercase">
+<a
+	href={`${base}/`}
+	class="h-32 ml-6 mr-auto mt-4 mb-6 inline-flex items-center justify-start uppercase"
+>
 	<D20 class="h-full w-auto mr-2" />
 	<h1 class="text-6xl font-bold">Scroll'd</h1>
 </a>
