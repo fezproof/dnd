eba546ecea70bdfc24cb40347482c07020f69009
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-10 23:09:38 +0800

    Test image removal

--- src/lib/components/Shards/ShardItem.svelte
+++ src/lib/components/Shards/ShardItem.svelte
@@ -12,9 +12,9 @@
 </script>
 
 <div class="campaign-link clip">
-	<div class="top-0 right-0 bottom-0 left-0" class:fixed class:absolute>
-		<img src={`${base}${image}`} alt={title} class="object-cover h-full w-full" />
-		<div class="tint" />
+	<div class="top-0 right-0 bottom-0 left-0 bg-red-500" class:fixed class:absolute>
+		<!-- <img src={`${base}${image}`} alt={title} class="object-cover h-full w-full" />
+		<div class="tint" /> -->
 	</div>
 	<div class="title-container">
 		<div class="title">
