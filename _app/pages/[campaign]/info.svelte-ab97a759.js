2533d05365dfabfff7a47261f3d7cb7f3c7f3f37
fezproof <26830309+fezproof@users.noreply.github.com>
2021-08-01 06:47:41 +0000

    Deploying to gh-pages from  @ b4d3016294fa294c223f0c42ba8f8e97efa9c29f 🚀

--- _app/pages/[campaign]/info.svelte-ab97a759.js
+++ _app/pages/[campaign]/info.svelte-ab97a759.js
@@ -1,1 +1,1 @@
-import{S as s,i as e,s as r}from"../../chunks/vendor-293ed57d.js";export default class extends s{constructor(s){super(),e(this,s,null,null,r,{})}}
+import{S as s,i as e,s as r}from"../../chunks/vendor-293ed57d.js";class t extends s{constructor(s){super(),e(this,s,null,null,r,{})}}export{t as default};
