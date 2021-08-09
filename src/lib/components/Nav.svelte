549aba2164c7761980afe0667ed0fcb3693545ac
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-08-09 19:54:07 +0800

    Made nav a little taller

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -7,7 +7,7 @@
 	<nav
 		class="sticky top-0 px-6 py-4 w-full bg-black bg-opacity-60 backdrop-blur-md flex items-center justify-center bg-clip-padding border-b-2 border-white/10"
 	>
-		<a href={`${base}/`} class="h-14 inline-flex items-center justify-start uppercase">
+		<a href={`${base}/`} class="h-16 inline-flex items-center justify-start uppercase">
 			<D20 class="h-full w-auto mr-3" />
 			<h1 class="text-3xl font-serif">Scroll'd</h1>
 		</a>
