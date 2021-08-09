1f11e2b0e3e59817e9aad65fe76455e17f10430c
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-08-09 21:27:11 +0800

    Cleaned up sizing more Added background on mobile

--- src/routes/[campaign]/index.svelte
+++ src/routes/[campaign]/index.svelte
@@ -19,68 +19,72 @@
 	<meta name="description" content={campaign?.excerpt} />
 </svelte:head>
 
-<div class={campaign?.font}>
-	<header class="mb-32 md:mb-56">
-		<CampaignHeroImage
-			imageUrl={campaign?.image}
-			imageAlt={`${campaign?.name} hero`}
-			imageHeight={900}
-			imageWidth={1600}
-		/>
-		<div class="min-h-screen flex flex-col px-8 pt-28 pb-8 justify-end items-start">
+<header>
+	<CampaignHeroImage
+		imageUrl={campaign?.image}
+		imageAlt={`${campaign?.name} hero`}
+		imageHeight={900}
+		imageWidth={1600}
+	/>
+	<div
+		class="min-h-screen flex flex-col pt-28 justify-end items-stretch md:items-start max-w-prose"
+	>
+		<div
+			class="px-8 pt-6 pb-16 bg-gradient-to-t from-black/80 via-black/80 to-transparent md:from-transparent md:via-transparent"
+		>
 			<h2
-				class="text-4xl md:text-7xl lg:text-8xl xl:text-9xl font-semibold border-b-8 border-orange-main pb-4 mb-8"
+				class={`text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold border-b-8 border-orange-main pb-4 mb-8 ${campaign?.font}`}
 			>
 				{campaign?.name}
 			</h2>
-			<p class="max-w-prose mb-6 font-sans prose md:prose-lg lg:prose-xl text-white">
+			<p class="mb-8 font-sans prose md:prose-lg lg:prose-xl text-white">
 				{@html campaign?.excerpt}
 			</p>
 			<a href={`${campaign?.link}/info`} class={classes['primary-button']}>Read more</a>
 		</div>
-	</header>
+	</div>
+</header>
 
-	<main>
-		<section class="section flex flex-col">
-			<h3 class="section-heading">The Misfits</h3>
-			{#if campaign?.players?.length}
-				<div
-					class="flex-1 h-full flex w-full items-stretch justify-center px-8 mx-auto max-w-screen-2xl"
-				>
-					<ShardsContainer>
-						{#each campaign?.players as player (player.id)}
-							<ShardItem
-								font={campaign?.font}
-								image={player.image}
-								link={player.link}
-								title={player.name}
-							/>
-						{/each}
-					</ShardsContainer>
-				</div>
-			{:else}
-				<p class="italic text-lg font-serif max-w-prose mx-auto">Coming soon...</p>
-			{/if}
-		</section>
-
-		<section class="section">
-			<h3 class="section-heading">The World</h3>
+<main>
+	<section class="section flex flex-col">
+		<h3 class={`section-heading ${campaign?.font}`}>The Misfits</h3>
+		{#if campaign?.players?.length}
+			<div
+				class="flex-1 h-full flex w-full items-stretch justify-center px-8 mx-auto max-w-screen-2xl"
+			>
+				<ShardsContainer>
+					{#each campaign?.players as player (player.id)}
+						<ShardItem
+							font={campaign?.font}
+							image={player.image}
+							link={player.link}
+							title={player.name}
+						/>
+					{/each}
+				</ShardsContainer>
+			</div>
+		{:else}
 			<p class="italic text-lg font-serif max-w-prose mx-auto">Coming soon...</p>
-		</section>
+		{/if}
+	</section>
 
-		<section class="section">
-			<h3 class="section-heading">Mission Logs</h3>
+	<section class="section">
+		<h3 class={`section-heading ${campaign?.font}`}>The World</h3>
+		<p class="italic text-lg font-serif max-w-prose mx-auto">Coming soon...</p>
+	</section>
 
-			{#if campaign?.logs?.length}
-				{#each campaign?.logs as log (log.id)}
-					<p class="italic text-lg font-serif max-w-prose mx-auto">{log.name}</p>
-				{/each}
-			{:else}
-				<p class="italic text-lg font-serif max-w-prose mx-auto">Coming soon...</p>
-			{/if}
-		</section>
-	</main>
-</div>
+	<section class="section">
+		<h3 class={`section-heading ${campaign?.font}`}>Mission Logs</h3>
+
+		{#if campaign?.logs?.length}
+			{#each campaign?.logs as log (log.id)}
+				<p class="italic text-lg font-serif max-w-prose mx-auto">{log.name}</p>
+			{/each}
+		{:else}
+			<p class="italic text-lg font-serif max-w-prose mx-auto">Coming soon...</p>
+		{/if}
+	</section>
+</main>
 
 <style lang="postcss">
 	.section {
